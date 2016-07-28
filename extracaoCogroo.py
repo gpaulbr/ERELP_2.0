cee3d8cb0d8bec706b7001c3b5faea5a3409aaa4
gpaulbr <gpaulbr@gmail.com>
2016-07-28 13:00:17 -0300

    adds output in console

--- extracaoCogroo.py
+++ extracaoCogroo.py
@@ -1,109 +1,114 @@
 # coding=utf-8
 
+
 def extracaoCogroo(diretorioTrabalho, categoriasUtilizadas, arquivo, dicFormatado):
-	vetorEntrada = list()
-	vetorIO = list()
-	dicionario = list()
-	
+    vetorEntrada = list()
+    vetorIO = list()
+    dicionario = list()
 
-	with open ('Profissao_Titulo.txt') as dicForm:				# Le o arquivo de dicionário e formata ele em um vetor
-		for linha in dicForm.readlines():						#
-			dicionario.append(linha[0:linha.find(';')])			#
+    # Le o arquivo de dicionário e formata ele em um vetor
+    with open('Profissao_Titulo.txt') as dicForm:
+        for linha in dicForm.readlines():
+            dicionario.append(linha[0:linha.find(';')])
 
-	with open(arquivo) as texto:
-		features = list()
-		gerarFeatureID = None
-		gerarFeatureNE = str()
+    with open(arquivo) as texto:
+        features = list()
+        gerarFeatureID = None
+        gerarFeatureNE = str()
 
-		for linha in texto.readlines():
-			dic = dict()
+        next(texto)
 
-			if linha[0] != 'I':
-		
-				lista = linha.split('\t')
-				dic["ID_S"] = (lista[0])
-				dic["ID"] = (lista[1])
+        for linha in texto:
+            dic = dict()
 
-				if dic["ID"] == 0:
-					features = list()
+            lista = linha.split('\t')
+            dic["ID_S"] = (lista[0])
+            dic["ID"] = (lista[1])
 
-				if lista[3] == '[]':
-		
-					dic["lemma"] = lista[2]
-		
-				else:
-		
-					dic["lemma"] = (lista[3][1:-1])
-		
-				dic["PoS"] = (lista[4])
-		
-				if lista[6] == 'O':
-		
-					dic["Head"] = True
-		
-				else:
-		
-					dic["Head"] = False
+            if dic["ID"] == 0:
+                features = list()
 
-				dic["NP"] = (lista[7])
-				dic["Structure"] = (lista[8])
+            if lista[3] == '[]':
 
-				if lista[9] != '-':
+                dic["lemma"] = lista[2]
 
-					dic["NE"] = (lista[9])
-		
-				else:
+            else:
 
-					dic["NE"] = 'null'
-				
-				if lista[10][:-1] != '-':
-					
-					features = lista[10][1:-2].split('|') 
-				
-				dic["gerarFeature"] = False
+                dic["lemma"] = (lista[3][1:-1])
 
-				if dic["ID"] in features:
+            dic["PoS"] = (lista[4])
 
-					vetorIO.append("I")
+            if lista[6] == 'O':
 
-					lemma = dic["lemma"].decode('utf-8').upper()			# transforma a string em unicode para o uppercase funcionar nas letras com acento
+                dic["Head"] = True
 
-					if lemma.encode('utf-8').replace('_', ' ') in dicionario:    # Substitui os _ por espaços na string e verifica se ela está no dicionário
-						dic["dicionario"] = True
+            else:
 
-					else: 
-						dic["dicionario"] = False
+                dic["Head"] = False
 
-				else:
-					dic["dicionario"] = False
-					vetorIO.append("O")
+            dic["NP"] = (lista[7])
+            dic["Structure"] = (lista[8])
 
-				vetorEntrada.append(dic)
+            if lista[9] != '-':
 
-				if dic["NE"] in categoriasUtilizadas:
+                dic["NE"] = (lista[9])
 
-					if gerarFeatureID == None:	
-						
-						gerarFeatureID = int(dic["ID"])
-						gerarFeatureNE = dic["NE"]
+            else:
 
-					else:
+                dic["NE"] = 'null'
 
-						if dic["NE"] != gerarFeatureNE:
-							
-							diferenca = int(dic["ID"]) - gerarFeatureID
-							
-							for index in range(vetorEntrada.index(dic)-diferenca, vetorEntrada.index(dic)+1):
-								vetorEntrada[index]["gerarFeature"] = True
+            if lista[10][:-1] != '-':
 
-	file = open('vetorDeEntrada.txt','w')
-	file.write(str(vetorEntrada))
-	file.close()
-	file = open('vetorIO.txt','w')
-	file.write(str(vetorIO))
-	file.close()
- 	for i in vetorEntrada:
- 		print i
+                features = lista[10][1:-2].split('|')
+
+            dic["gerarFeature"] = False
+
+            if dic["ID"] in features:
+
+                vetorIO.append("I")
+
+                # transforma a string em unicode para o uppercase funcionar nas
+                # letras com acento
+                lemma = dic["lemma"].decode('utf-8').upper()
+
+                # Substitui os _ por espaços na string e verifica se ela está
+                # no dicionário
+                if lemma.encode('utf-8').replace('_', ' ') in dicionario:
+                    dic["dicionario"] = True
+
+                else:
+                    dic["dicionario"] = False
+
+            else:
+                dic["dicionario"] = False
+                vetorIO.append("O")
+
+            vetorEntrada.append(dic)
+
+            if dic["NE"] in categoriasUtilizadas:
+
+                if gerarFeatureID is None:
+
+                    gerarFeatureID = int(dic["ID"])
+                    gerarFeatureNE = dic["NE"]
+
+                else:
+
+                    if dic["NE"] != gerarFeatureNE:
+
+                        diferenca = int(dic["ID"]) - gerarFeatureID
+
+                        for index in range(vetorEntrada.index(dic)-diferenca, vetorEntrada.index(dic)+1):
+                            vetorEntrada[index]["gerarFeature"] = True
+
+    file = open('vetorDeEntrada.txt', 'w')
+    file.write(str(vetorEntrada))
+    file.close()
+    file = open('vetorIO.txt', 'w')
+    file.write(str(vetorIO))
+    file.close()
+    for i in vetorEntrada:
+        print i
 
 categorias = ['PES', 'ORG', 'LOC']
 dicionarios = ['./Profissao_Titulo.txt']
