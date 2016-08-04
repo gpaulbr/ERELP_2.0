81e62948c3713e3ed06d39849fe0556c4a19f9fc
gpaulbr <gpaulbr@gmail.com>
2016-08-04 16:25:49 -0300

    adds the number of relations

--- extracaoCogroo.py
+++ extracaoCogroo.py
@@ -1,10 +1,14 @@
 # coding=utf-8
 
+import time
+import os
 
 def extracaoCogroo(diretorioTrabalho, categoriasUtilizadas, arquivo, vetDicionarios):
     vetorEntrada = list()
     vetorIO = list()
+    # Dicionario com as palavras dos arquivos de dicionarios passados para o método
     dic = dict()
+    numeroRelacoes = 0
 
     # Le os txts de dicionarios e formata eles no dic
     for dicionario in vetDicionarios:
@@ -20,6 +24,7 @@ def extracaoCogroo(diretorioTrabalho, categoriasUtilizadas, arquivo, vetDicionar
         next(texto)
 
         for linha in texto:
+
             dicPalavra = dict()
 
             lista = linha.split('\t')
@@ -67,9 +72,9 @@ def extracaoCogroo(diretorioTrabalho, categoriasUtilizadas, arquivo, vetDicionar
             # transforma a string em unicode para o uppercase funcionar nas
             # letras com acento
             lemma = dicPalavra["lemma"].decode('utf-8').upper()
-            lemma = lemma.encode('utf-8').replace('_', ' ')
             # Substitui os _ por espaços na string e verifica se ela está
             # no dicionário
+            lemma = lemma.encode('utf-8').replace('_', ' ')
             try:
                 dic[lemma]
                 dicPalavra["dicionario"] = True
@@ -80,6 +85,7 @@ def extracaoCogroo(diretorioTrabalho, categoriasUtilizadas, arquivo, vetDicionar
             if dicPalavra["ID"] in features:
 
                 vetorIO.append("I")
+                numeroRelacoes += 1
 
             else:
                 vetorIO.append("O")
@@ -102,15 +108,32 @@ def extracaoCogroo(diretorioTrabalho, categoriasUtilizadas, arquivo, vetDicionar
                         for index in range(vetorEntrada.index(dicPalavra)-diferenca, vetorEntrada.index(dicPalavra)+1):
                             vetorEntrada[index]["gerarFeature"] = True
 
-    file = open('vetorDeEntrada.txt', 'w')
-    file.write(str(vetorEntrada))
+    os.mkdir(diretorioTrabalho)
+    file = open(diretorioTrabalho+'/vetorDeEntrada.txt', 'a')
+    file.write(str(vetorEntrada)+'\n')
     file.close()
-    file = open('vetorIO.txt', 'w')
-    file.write(str(vetorIO))
+    file = open(diretorioTrabalho+'/vetorIO.txt', 'a')
+    file.write(str(vetorIO)+'\n')
     file.close()
+
     for i in vetorEntrada:
         print i
 
-categorias = ['PES', 'ORG', 'LOC']
-dicionarios = ['../Dicionarios/Profissao_Titulo.txt', '../Dicionarios/Localizacao.txt']
-extracaoCogroo('./', categorias, 'ric-42664.cg', dicionarios)
+    return vetorEntrada, numeroRelacoes
+
+displaytime = time.strftime("%x %X", time.localtime())
+diretorioTrabalho = './saida_ORG_PES_Gabriel'+'_'+str(displaytime.replace('/', '-'))
+categorias = ['PES', 'ORG']
+dicionarios = ['../Dicionarios/Profissao_Titulo.txt']  # , '../Dicionarios/Localizacao.txt']
+
+extracaoCogroo(diretorioTrabalho, categorias, 'ric-42664.cg', dicionarios)
+
+
+# def comparar(x, y):
+
+#     if x.lower() > y.lower():
+#         return 1
+#     elif x.lower() == y.lower():
+#         return 0
+#     else:
+#         return -1
